0be37051ffe4d5cc3980a2316f9d88d1256ce6d5
paulo maia <pauloafmaia9@gmail.com>
2023-04-17 21:59:07 -0300

    Rota inicial do app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (request, response) {
+  response.send("Rota padrão");
+});
+
+app.listen(3000, function () {
+  console.log("O aplicativo está rodando na porta 3000");
+});
