59e5963d2b77c6672db62a57aaa5ba9e48a77d2f
Felipe Reinaux <felipereinaux@gmail.com>
2023-04-17 21:59:06 -0300

    rota inicial do app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+const express = require('express')
+
+const app = express()
+
+// get, post, put, delete, patch
+app.get('', function (requisicao, resposta) {
+  resposta.send('Rota padrão')
+})
+
+app.listen(3000, function () {
+  console.log('O aplicativo está rodando na porta 3000')
+})
