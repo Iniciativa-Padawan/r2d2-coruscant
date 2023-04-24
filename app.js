d0091537b463b6a75a62c7aee96af42aed5e5390
Felipe Reinaux <felipereinaux@gmail.com>
2023-04-24 19:53:32 -0300

    coleção do thunder client

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express')
 
 const app = express()
 
+const movies = [
+  {
+    name: 'O Império Contra-Ataca',
+    year: 1981
+  },
+  {
+    name: 'A ameaça fanstasma',
+    year: 1999
+  }
+]
+
 // get, post, put, delete, patch
 app.get('', function (requisicao, resposta) {
   resposta.send('Rota padrão')
+})
+
+app.get('/movies', (req, res) => {
+  res.send(movies)
 })
 
 app.listen(3000, function () {
