2dfc68eb6b46fb9b52d5304d6e0d6c73f1646431
Felipe Reinaux <felipereinaux@gmail.com>
2023-05-08 20:14:11 -0300

    port env

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const movieRoutesTopic = require('./topic/movies/routes')
 var cors = require('cors')
+const port = process.env.PORT || 3001;
 
 const app = express()
 app.options('*', cors())
 app.use(bodyParser.json())
 app.use(movieRoutesTopic)
 
-app.listen(3000, function () {
-  console.log('O aplicativo está rodando na porta 3000')
+app.listen(port, function () {
+  console.log(`O aplicativo está rodando na porta ${port}`)
 })
 
 
