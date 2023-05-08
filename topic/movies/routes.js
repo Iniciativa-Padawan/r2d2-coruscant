68bdbfba4900d2559e164da0f136b6d8a5a8a783
paulo maia <pauloafmaia9@gmail.com>
2023-05-08 11:30:26 -0300

    Update routes with new methods

--- topic/movies/routes.js
+++ topic/movies/routes.js
@@ -4,60 +4,161 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const schemas = require("./schemas");
 
+/* GET METHODS */
+
 router.get("/movies", async (req, res) => {
-  const movies = await controller.getMovies();
-  res.send(movies);
+  const movies = await controller.getAllMovies();
+
+  if (movies.length > 0) {
+    res.send(movies);
+  } else {
+    res.status(404).send("Movies not found");
+  }
 });
 
-router.get("/movies/trilogy/:trilogy", (req, res) => {
-  const movies = controller.getMoviesBytrilogy(req.params.trilogy);
+router.get("/movies/:id", async (req, res) => {
+  const movie = await controller.getMovieById(req.params.id);
 
-  if (movies.length == 0) res.status(404).send("Trilogy not found");
-  else res.send(movies);
+  if (!movie) {
+    res.status(404).send("Movie not found");
+  } else {
+    res.send(movie);
+  }
 });
 
-router.get("/movies/alphabetical", (req, res) => {
-  res.send(controller.ordemAlfabetica());
+router.get("/movies/:name", async (req, res) => {
+  const movie = await controller.getMovieByName(req.params.name);
+
+  if (!movie) {
+    res.status(404).send("Movie not found");
+  } else {
+    res.send(movie);
+  }
 });
 
-router.get("/movies/sequential", (req, res) => {
-  res.send(controller.ordemsequential());
+router.get("/movies/year/:year", async (req, res) => {
+  const movie = await controller.getMovieByYear(req.params.year);
+
+  if (!movie) {
+    res.status(404).send("Movie not found");
+  } else {
+    res.send(movie);
+  }
 });
 
-router.get("/movies/movie-release", (req, res) => {
-  res.send(controller.ordemLancamento());
+router.get("/movies/sequential/:sequential", async (req, res) => {
+  const movie = await controller.getMovieBySequential(req.params.sequential);
+
+  if (!movie) {
+    res.status(404).send("Movie not found");
+  } else {
+    res.send(movie);
+  }
 });
+
+router.get("/movies/trilogy/:trilogy", async (req, res) => {
+  const movies = await controller.getMoviesByTrilogy(req.params.trilogy);
+
+  if (movies.length == 0) {
+    res.status(404).send("Trilogy not found");
+  } else {
+    res.send(movies);
+  }
+});
+
+router.get("/movies/orderbyname/asc", async (req, res) => {
+  const movies = await controller.getMoviesOrderByNameAsc();
+
+  if (movies.length > 0) {
+    res.send(movies);
+  } else {
+    res.status(404).send("Movie not found");
+  }
+});
+
+router.get("/movies/orderbyname/desc", async (req, res) => {
+  const movies = await controller.getMoviesOrderByNameDesc();
+
+  if (movies.length > 0) {
+    res.send(movies);
+  } else {
+    res.status(404).send("Movie not found");
+  }
+});
+
+router.get("/movies/orderbyyear/asc", async (req, res) => {
+  const movies = await controller.getMoviesOrderByYearAsc();
+
+  if (movies.length > 0) {
+    res.send(movies);
+  } else {
+    res.status(404).send("Movie not found");
+  }
+});
+
+router.get("/movies/orderbyyear/asc", async (req, res) => {
+  const movies = await controller.getMoviesOrderByYearDesc();
+
+  if (movies.length > 0) {
+    res.send(movies);
+  } else {
+    res.status(404).send("Movie not found");
+  }
+});
+
+router.get("/movies/orderbysequential/asc", async (req, res) => {
+  const movies = await controller.getMoviesOrderBySequentialAsc();
+
+  if (movies.length > 0) {
+    res.send(movies);
+  } else {
+    res.status(404).send("Movie not found");
+  }
+});
+
+router.get("/movies/orderbysequential/asc", async (req, res) => {
+  const movies = await controller.getMoviesOrderBySequentialDesc();
+
+  if (movies.length > 0) {
+    res.send(movies);
+  } else {
+    res.status(404).send("Movie not found");
+  }
+});
+
+/* POST METHODS */
 
 router.post("/movies", schemas.movieSchema(), async (req, res) => {
   const result = validationResult(req);
   if (result.errors.length > 0) {
     res.status(400).send({ errors: result.errors.map((erro) => erro.msg) });
   } else {
-    await controller.addMovie(req.body);
-    res.status(201).send(controller.getMovies());
+    await controller.createMovie(req.body);
+    res.status(201).send(controller.getAllMovies());
   }
 });
 
-router.put("/movies/:id", schemas.movieSchema(), (req, res) => {
+/* PUT METHODS */
+
+router.put("/movies/:id", schemas.movieSchema(), async (req, res) => {
   const result = validationResult(req);
   if (result.errors.length > 0) {
     res.status(400).send({ errors: result.errors.map((erro) => erro.msg) });
   } else {
-    controller.updateMovie(req.body, req.params.sequential);
-    res.send(controller.getMovies());
+    await controller.updateMovie(req.body, req.params.id);
+    const movies = await controller.getAllMovies();
+    res.status(200).send(movies);
   }
 });
 
+/* DELETE METHODS */
+
 router.delete("/movies/:id", async (req, res) => {
-  controller.ById(req.params.id);
+  await controller.deleteMovie(req.params.id);
 
-  res.send(controller.getMovies());
-});
+  const movies = await controller.getAllMovies();
 
-router.delete("/movies", (req, res) => {
-  controller.deleteLastMovie();
-
-  res.send(controller.getMovies());
+  res.status(200).send(movies);
 });
 
 module.exports = router;
